0aaebdacff573abad6e285f20c1d55d69fd00157
Tommie <tommie.rundberg@gmail.com>
2022-02-09 21:59:22 +0100

    update

---
+++ src/components/loggedin.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+   <h2>Inloggad!/h2><br>
+       </div>
+
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+
+.container  {
+height:100%;
+position:fixed;
+top:70px;
+left:0px;
+padding:20px;
+padding-top:50px;
+background-color:white;
+width:calc(100% - 40px);
+font-size:25px;
+text-align:center;
+font-family: 'Roboto', sans-serif;
+} 
+
+.input {
+ height: 30px;
+ width: 200px;
+font-size:16px;
+}
+</style>
+
+
+
+<script>
+
+</script>
