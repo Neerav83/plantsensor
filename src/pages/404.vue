f3c25a07a466b4e7cfa3b5392df32ae627cd67bd
Tommie <tommie.rundberg@gmail.com>
2022-02-09 22:10:18 +0100

    update

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,11 +1,11 @@
 <template>
   <main-layout>
-    <p>Page not fount</p>
+    <p>Page not found</p>
   </main-layout>
 </template>
 
 <script>
-  import MainLayout from '/layouts/Main.vue'
+  import MainLayout from './layouts/Main.vue'
   export default {
     components: {
       MainLayout
