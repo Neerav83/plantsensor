827ab59bf4746e73d884f0df65adc4b136db2600
Tommie <tommie.rundberg@gmail.com>
2022-02-09 21:34:21 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,15 @@
   <Header msg="Plantsensor" />
   
   <Container  />
+
 </div>
 </template>
 
 <script>
 import Header from './components/header.vue'
 import Container from './components/body.vue'
+
+console.log(new URL(location.href).searchParams.get('username'));
 
 export default {
   name: 'App',
