9cd3f71ad8974c6f9deaf9ca1ce468fa4da0e529
Tommie <tommie.rundberg@gmail.com>
2022-02-09 22:05:04 +0100

    update

---
+++ src/layouts/Main.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+  <Header msg="Plantsensor" />
+  <Container  />
+
+
+</div>
+</template>
+
+<script>
+import Header from './components/header.vue'
+import Container from './components/body.vue'
+
+
+
+export default {
+  name: 'App',
+  components: {
+    'Header':Header,
+    'Container':Container	
+
+  }
+}
+
+</script>
+
+
