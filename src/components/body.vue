593d5a72246249424aea6caed1ced9cc59891a11
Tommie <tommie.rundberg@gmail.com>
2022-02-09 17:24:26 +0100

    update

--- src/components/body.vue
+++ src/components/body.vue
@@ -59,7 +59,7 @@ export default {
       axios.post('http://192.168.68.107:8000/checklogin', this.formdata )
       .then(res => {
        // console.log("Resultat:")
-       // console.log(res)
+        console.log(res)
         if (res=="1"){
           alert("Success!!")
         }
