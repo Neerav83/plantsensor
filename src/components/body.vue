92bf223037e0edc4048760641ea78b2dcc604f0b
Tommie Rundberg <tommie.rundberg@gmail.com>
2022-02-09 11:36:46 +0100

    added

---
+++ src/components/body.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'container',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+
+.container  {
+height:100%;
+position:fixed;
+top:70px;
+left:0px;
+padding:20px;
+padding-top:50px;
+background-color:white;
+width:100%;
+color:white;
+font-size:25px;
+text-align:center;
+font-family: 'Roboto', sans-serif;
+} 
+
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'PlantSensor',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+
+.header  {
+height:30px;
+position:fixed;
+top:0px;
+left:0px;
+padding:20px;
+background-color:green;
+width:100%;
+color:white;
+font-size:25px;
+text-align:center;
+font-family: 'Roboto', sans-serif;
+} 
+
+</style>
