4fc96880c99f19357375c282947a9ec16fb0aeb1
Tommie <tommie.rundberg@gmail.com>
2022-02-09 22:12:22 +0100

    update

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -5,10 +5,12 @@
 </template>
 
 <script>
-  import MainLayout from './layouts/Main.vue'
+/*
+import MainLayout from './layouts/Main.vue'
   export default {
     components: {
       MainLayout
     }
   }
+  */
 </script>
